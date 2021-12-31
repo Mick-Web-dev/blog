d771d09af9b8e88f57f27cc097d6e3478220fe9c
mickael <mickael.hesliere@gmail.com>
2021-12-31 07:16:41 +0100

    remove composer installation

--- views/viewAccueil.php
+++
@@ -1,8 +0,0 @@
-<?php
-foreach ($posts as $post):
-?>
-<h2><?= $post->titre() ?></h2>
-<p><?= $post->chapo() ?></p>
-<time><?= $post->date() ?></time>
-    <p><?= $post->auteur() ?></p>
-<?php endforeach; ?>
