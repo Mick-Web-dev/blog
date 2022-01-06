a074572d8927bad7f2f0db8652b2f290d55afbeb
mickael <mickael.hesliere@gmail.com>
2022-01-06 17:51:05 +0100

    refactoring database.php with Singleton for instances request limit to one connection if it open #4

--- libraries/Database.php
+++ libraries/Database.php
@@ -1,16 +1,21 @@
 <?php
 // Ce fichier concerne les fonctionnalités liées à la base de données
 class Database {
+    private static $instance = null;
     //1- Fonctionnalités servant de connexion à la bdd.
     /**
      * Retourne une connexion à la base de données
      * @return PDO
      */
-    public static function getPdo(): PDO {
-        $pdo = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
-        return $pdo;
+    public static function getPdo(): PDO
+    {
+        if (self::$instance === null) {
+            self::$instance = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+
+        }
+        return self::$instance;
     }
 }
